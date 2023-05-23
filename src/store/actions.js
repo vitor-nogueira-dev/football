64e4ab65760a2a961a5c77e77b8f38bf739e1c5c
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 11:23:06 -0300

    feat: ""
    implement action setCountries

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,15 @@
-import { SET_API_KEY } from "../utils/constantes";
+import { SET_API_KEY, SET_COUNTRIES } from '../utils/constantes';
 
 export const ACTION_SET_API_KEY = (API_KEY) => {
   return {
     type: SET_API_KEY,
     payload: API_KEY,
+  };
+};
+
+export const ACTION_SET_COUNTRIES = (countries) => {
+  return {
+    type: SET_COUNTRIES,
+    payload: countries,
   };
 };
