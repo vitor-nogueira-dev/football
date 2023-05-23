a21ee80c4f662ac31cea2f9fceacc52796a01d42
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 12:07:12 -0300

    fix:
    organizing imports in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Switch } from 'react-router-dom'
 
 import './App.css'
+import Home from './pages/Home/Home'
+import Countries from './pages/Countries/Countries'
+import Leagues from './pages/Leagues/Leagues'
 
 function App() {
 
@@ -9,8 +12,8 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route path="/countries" component={Countries}/>
       <Route path="/leagues/:country" component={Leagues}/>
-      <Route path="/teams/:id" component={Teams}/>
-      <Route path="/players/:id" component={Players}/>
+      {/* <Route path="/teams/:id" component={Teams}/>
+      <Route path="/players/:id" component={Players}/> */}
     </Switch>
   )
 }
