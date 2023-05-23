5b168cb17dbd1cb01ff42ff29d37e81e6f0f3341
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 13:06:29 -0300

    feat: ""
    implement component CardPlayer

---
+++ src/components/Players/Players.jsx
@@ -0,0 +1,13 @@
+
+const CardPlayer = (data) => {
+  return (
+    <CardContainer>
+      <NamePlayer>{data.player.name}</NamePlayer>
+      <AgePlayer>Age: {data.player.age}</AgePlayer>
+      <Nationality>Nationality: {data.player.nationality}</Nationality>
+      <Img src={data.player.photo} alt={data.player.name} />
+    </CardContainer>
+  )
+}
+
+export default CardPlayer;
