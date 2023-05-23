15a9567c5b8af838f3f24b4f798a48060c467c88
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 11:48:04 -0300

    style: ""
    add style page Countries

--- src/pages/Countries/Countries.jsx
+++ src/pages/Countries/Countries.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { CardCountry } from "../../components/Countries/Countries";
+import Container from "./styles";
 
 export const Countries = () => {
   const countries = useSelector((state) => state.countries);
---
+++ src/pages/Countries/styles.js
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export default Container;
