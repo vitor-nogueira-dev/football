863c8e8c84d2c389ce2f54a44d6ceed5c7fef3c9
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 11:51:23 -0300

    feat: ""
    implement page Leagues

---
+++ src/pages/Leagues/Leagues.jsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom";
+import { useCallback, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { fetchAPI } from "../../store/actions";
+
+export const Leagues = () => {
+  const dispatch = useDispatch()
+  const leagues = useSelector((state) => state.leagues);
+
+  const { country } = useParams();
+
+  const fetchLeagues = useCallback(async () => {
+    dispatch(fetchAPI('leaguesOfCountry', country, '', '', ''));
+  }, [country, dispatch]);
+
+  useEffect(() => {
+    fetchLeagues();
+  }, [fetchLeagues]);
+
+  return (
+    <>
+      {/* <Title title="Selecione a liga/copa para visualizar os times" /> */}
+      <Container>
+        {leagues?.map((league) => {
+          return <CardLeagues
+            key={league.league.id}
+            {...league} />
+        })}
+      </Container>
+    </>
+  )
+}
