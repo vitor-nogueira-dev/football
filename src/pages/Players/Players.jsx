b8f4e8d5a26a7c1fca8d3fe9cb51b8a0d5a49a2f
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 12:14:32 -0300

    feat: ""
    implement page Players

---
+++ src/pages/Players/Players.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { ACTION_FETCH_API } from "../../store/actions";
+import { Games } from "../../components/Games/Games";
+import ChartComponent from "../../components/Chart/Chart";
+import Title from "../../components/Title/Title";
+
+export const Players = () => {
+  const { id: idTeam } = useParams();
+  const dispatch = useDispatch();
+
+  const players = useSelector((state) => state.players);
+  const teamStatistic = useSelector((state) => state.teamStatistic);
+  const idLeague = useSelector((state) => state.idLeague);
+  const season = useSelector((state) => state.season);
+
+  const fetchPlayers = useCallback(async () => {
+    dispatch(ACTION_FETCH_API('players', '', '', season, idTeam));
+    dispatch(ACTION_FETCH_API('teamStatistic', '', idLeague || '', season, idTeam));
+  }, [dispatch, idTeam, idLeague, season]);
+
+  useEffect(() => {
+    fetchPlayers();
+  }, [fetchPlayers]);
+
+  return (
+    <>
+      <Title title="" />
+      <Container>
+        <ContentInfos>
+          {teamStatistic?.fixtures && <Games />}
+          <ChartComponent />
+        </ContentInfos>
+        {players?.map((player) => {
+          return <CardPlayer key={player.player.id} {...player} />;
+        })}
+      </Container>
+    </>
+  )
+}
