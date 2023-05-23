608e7bb2c652a332e6e88e401692837d62209738
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 19:34:22 -0300

    feat:
    create component CardTeam

---
+++ src/components/Teams/CardTeam.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+import { Button, LogoCell, LogoImage, Table, TableCell, TableHeader, TableRow } from "../../styles/styles.global"
+
+const CardTeams = (data) => {
+  return (
+    <Link to={`/players/${data.team.id}`}>
+      <Button>
+        <Table>
+          <tbody>
+            <TableRow>
+              <TableHeader>Nome</TableHeader>
+              <TableHeader>País</TableHeader>
+              {/* <TableHeader>Fundação</TableHeader> */}
+              {data.team.founded && <TableHeader>Fundação</TableHeader>}
+              <TableHeader>Logo</TableHeader>
+            </TableRow>
+            <TableRow>
+              <TableCell>{data.team.name}</TableCell>
+              <TableCell>{data.team.country}</TableCell>
+              {data.team.founded && <TableCell>{data.team.founded}</TableCell>}
+              <LogoCell>
+                <LogoImage src={data.team.logo} alt={data.team.name} />
+              </LogoCell>
+            </TableRow>
+          </tbody>
+        </Table>
+      </Button>
+    </Link>
+  )
+}
+
+export default CardTeams;
