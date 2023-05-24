8788a90c6d47b4ccc7b65ee5af58b4d623fcb32b
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 21:41:11 -0300

    fix:
    add title Countries

--- src/pages/Countries/Countries.jsx
+++ src/pages/Countries/Countries.jsx
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux";
 import { CardCountry } from "../../components/Countries/CardCountry";
 import Container from "./styles";
+import Title from "../../components/Title/Title";
 
 const Countries = () => {
   const countries = useSelector((state) => state.countries);
 
   return (
     <>
+    <Title title="Selecione um País para visualizar as ligas" fontSize="1.5rem"/>
       <Container>
         {countries?.map((country) => (
           <CardCountry
