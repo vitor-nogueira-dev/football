5e9b5fac341af0dda772c5ce6ba02b44a248ae1a
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-24 09:41:00 -0300

    fix:
    removing files

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </Provider>,
+    </Provider>
   </BrowserRouter>
 )
