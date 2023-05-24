df7593cbd56ea53bb0a6b1620e14dba39edecd1a
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-23 22:27:40 -0300

    feat:
    create globalState to tests

---
+++ tests/helpers/globalState.js
@@ -0,0 +1,13 @@
+// import { questionsResponse } from './mocks';
+
+const initialState = {
+  clubReducer: {
+    API_KEY: '',
+    countries: [],
+    leagues: [],
+    teams: [],
+    players: [],
+  },
+};
+
+export default initialState;
